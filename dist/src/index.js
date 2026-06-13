7e3daabdf71fa5afd9ad7a7ab41f373472ed11b0
Adam Krieger <adamkrieger87@gmail.com>
2026-06-12 21:41:44 -0400

    Deploy

--- dist/src/index.js
+++ dist/src/index.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
 const app_1 = __importDefault(require("./app"));
-const PORT = parseInt(process.env.PORT || '3000', 10);
-app_1.default.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is running on port ${PORT}`);
+app_1.default.listen(3000, '0.0.0.0', () => {
+    console.log(`Server is running on port 3000`);
 });
